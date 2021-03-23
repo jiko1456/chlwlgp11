fa6adab32b660f3101018a449e0ccd21496166d7
jiko1456 <chlwlgp11@naver.com>
2021-03-23 16:50:20 +0900

    화면 구상

--- app/src/main/java/com/example/lectureroom/LectureRoom.java
+++ app/src/main/java/com/example/lectureroom/LectureRoom.java
@@ -1,4 +1,26 @@
 package com.example.lectureroom;
 
-public class LectureRoom {
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class LectureRoom extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lecturerooom);
+
+        Button button=findViewById(R.id.button01);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
 }
